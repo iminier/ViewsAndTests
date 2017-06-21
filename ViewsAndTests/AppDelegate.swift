a5d064fde1653282a639b3ec26732964ba178bee
Ivan Minier <ivanminier@me.com>
2017-06-20 17:47:22 -0700

    Initial Commit

--- ViewsAndTests/AppDelegate.swift
+++ ViewsAndTests/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let currentViewController = ViewController()
+        
+        window?.rootViewController = currentViewController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
